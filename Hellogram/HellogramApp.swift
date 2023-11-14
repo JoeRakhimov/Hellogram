278af0274051bf01cd4c29c8e0029d7070b17a4a
Joe <joerakhimov@gmail.com>
2023-11-14 14:58:26 +0100

    Initial Commit

---
+++ Hellogram/HellogramApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellogramApp.swift
+//  Hellogram
+//
+//  Created by Gayrat Rakhimov on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HellogramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
